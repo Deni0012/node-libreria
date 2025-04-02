1b7d78b98ab49a2a67aa5bf69cfb82da363e581f
Deni0012 <deni.dg00@icloud.com>
2025-04-02 10:13:17 +0200

    feat: created new access for server

---
+++ app.js
@@ -0,0 +1,11 @@
+//richieste
+const express = require("express")
+//app
+const app = express()
+const port = 5500
+
+
+
+
+
+app.listen(port, function () { console.log(`Server Attivo con la porta ${port}`) })
